446e1115f4d012e1b64156a2bf5b82aebd35a8e4
Gregmal <gregdu131134@gmail.com>
2018-03-26 16:30:44 +0200

    contact add

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -17,8 +17,8 @@ export class ContactPage {
   addContact(){
   let contact : Contact = this.contacts.create(); 
     contact.name = new ContactName(null, 'Ecole', 'LePetitPrince');
-	  //contact.phoneNumbers = [new ContactField('mobile', '0412345678')];
-	  //contact.emails = [new ContactField('emails','contact@lepetitprince.fr')];
+	  contact.phoneNumbers = [new ContactField('mobile', '0412345678')];
+	  contact.emails = [new ContactField('emails','contact@lepetitprince.fr')];
 	  contact.save().then(
       () => console.log('Contact saved!', contact),
       (error: any) => console.error('Error saving contact.', error)
